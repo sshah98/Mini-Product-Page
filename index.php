2851bf40d8885a72a116911986e0a531fe3549f8
sshah98 <shahsuraj261@gmail.com>
2018-02-20 00:39:26 -0500

    working shopping cart now

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ if (isset($_POST["add_to_cart"])) {
         'id'=>$_GET["id"],
         'name'=>$_POST["hidden_name"],
         'price'=>$_POST["hidden_price"],
+        'category'=>$_POST["hidden_category"],
         'quantity'=>$_POST["quantity"]
       );
       //keeps an indexed count in this session to keep track of how many items were added to cart
@@ -32,10 +33,22 @@ if (isset($_POST["add_to_cart"])) {
       'id'=>$_GET["id"],
       'name'=>$_POST["hidden_name"],
       'price'=>$_POST["hidden_price"],
+      'category'=>$_POST["hidden_category"],
       'quantity'=>$_POST["quantity"]
     );
     //store all the data into the itemArray array
     $_SESSION["shoppingCart"][0] = $itemArray;
+  }
+}
+
+
+if (isset($_GET["action"])) {
+  if ($_GET["action"] == "delete") {
+    foreach ($_SESSION["shoppingCart"] as $keys=>$values) {
+      if ($values["id"] == $_GET["id"]) {
+        unset($_SESSION["shoppingCart"][$keys]);
+      }
+    }
   }
 }
 
@@ -52,7 +65,7 @@ if (isset($_POST["add_to_cart"])) {
 <html>
 <head>
   <title>Suraj's Products</title>
-  <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>   -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />  
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
 </head>
@@ -68,23 +81,23 @@ if (isset($_POST["add_to_cart"])) {
     $query = "SELECT * FROM productInfo ORDER BY id ASC";
     $result = mysqli_query($connection, $query);
     
-    $query2 = "SELECT * FROM productPic ORDER BY id ASC";
-    $result2 = mysqli_query($connection, $query2);
+    // $query2 = "SELECT * FROM productPic ORDER BY id ASC";
+    // $result2 = mysqli_query($connection, $query2);
     //check to see if the database has any rows
-    if (mysqli_num_rows($result) > 0 and mysqli_num_rows($result2)) {
+    if (mysqli_num_rows($result) > 0) {
       
       //fetch all the data of the database
-      while ($row = mysqli_fetch_array($result) and $row2 = mysqli_fetch_array($result2)) {
+      while ($row = mysqli_fetch_array($result)) {
         ?>
         
         <form method="post" action="index.php?action=add&id=<?php echo $row["id"]; ?>">
           
-          <img src="<?php echo $row2["image"]; ?>" class="img-responsive" width="400" height="400" />
           <br />
           <h4><?php echo $row["name"]; ?> </h4>
           <h4>$<?php echo $row["price"]; ?> </h4>
           <h4>Category: <?php echo $row["category"]; ?> </h4>
-          <input type="text" name="quantity" class="form-control" value="1" />  
+          <input type="number" name="quantity" class="form-control" value="1" min="1"/>  
+          <input type="hidden" name="hidden_category" value="<?php echo $row["category"]; ?>" />
           <input type="hidden" name="hidden_name" value="<?php echo $row["name"]; ?>" />  
           <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>" />  
           <br />
@@ -108,6 +121,7 @@ if (isset($_POST["add_to_cart"])) {
         <th>Item Name</th>
         <th>Quantity</th>
         <th>Price</th>
+        <th>Category</th>
         <th>Total</th>
         <th>Action</th>
       </tr>
@@ -122,9 +136,10 @@ if (isset($_POST["add_to_cart"])) {
         <tr>
           <td><?php echo $values["name"]; ?> </td>
           <td><?php echo $values["quantity"]; ?> </td>
-          <td>$ <?php echo $values["prices"]; ?> </td>
-          <td> <?php echo $values["quantity"] * $values["prices"]; ?> </td>
-          <td><a href="index.php?action=delete&id=<?php echo $values["item_id"]; ?> "</a></td>
+          <td>$ <?php echo $values["price"]; ?> </td>
+          <td> <?php echo $values["category"]; ?></td>
+          <td> <?php echo $values["quantity"] * $values["price"]; ?> </td>
+          <td><a href="index.php?action=delete&id=<?php echo $values["id"]; ?>"><span class="text-danger">Remove</span></a></td>  
           
         </tr>
         <?php 
