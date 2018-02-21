7fd0194341ee325e343a0a1cc96d8c587018514d
sshah98 <shahsuraj261@gmail.com>
2018-02-21 00:20:49 -0500

    added quantity to database

--- productInfo.sql
+++ productInfo.sql
@@ -1,42 +1,43 @@
 CREATE TABLE IF NOT EXISTS `productInfo` (
-  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `id` Int NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `code` varchar(50) NOT NULL,
   `price` Int NOT NULL, 
   `category` varchar(50) NOT NULL,
+  `quantity` Int NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY product_code (`code`)
 );
 
 CREATE TABLE IF NOT EXISTS `productPic` (
-  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `id` Int NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `image` text NOT NULL,
   PRIMARY KEY (`id`)
 );
 
 INSERT INTO `productInfo` (`id`, `name`, `code`, `price`, `category`) VALUES
-(1, 'Apple', 'Ac3401', 4, 'grocery'),
-(2, 'Banana', 'Bc3822', 3, 'grocery'),
-(3, 'Bread', 'Br4281', 2, 'grocery'),
-(4, 'Chips', 'Ch2132', 3, 'grocery'),
-(5, 'Juice', 'Ju8762', 2, 'grocery'),
-(6, 'Milk', 'Mi4562', 3, 'grocery'),
-(7, 'Tomato', 'To6642', 2, 'grocery'),
-(8, 'Camera', 'Ca7782', 200, 'technology'),
-(9, 'Headphones', 'He6252', 50, 'technology'),
-(10, 'Laptop', 'La8192', 500, 'technology'),
-(11, 'Phone', 'Ph0019', 300, 'technology'),
-(12, 'Printer', 'Pr8928', 150, 'technology'),
-(13, 'Speakers', 'Sp0218', 50, 'technology'),
-(14, 'TV', 'Tv0123', 350, 'technology'),
-(15, 'Hoodie', 'Ho7721', 15, 'clothes'),
-(16, 'Jacket', 'Ja0987', 25, 'clothes'),
-(17, 'Jeans', 'Je2121', 20, 'clothes'),
-(18, 'Shirt', 'Sh6517', 15, 'clothes'),
-(19, 'Shoes', 'Se0011', 30, 'clothes'),
-(20, 'Shorts', 'Sr0982', 25, 'clothes'),
-(21, 'Sunglasses', 'Su3321', 45, 'clothes');
+(1, 'Apple', 'Ac3401', 4, 'grocery', 20),
+(2, 'Banana', 'Bc3822', 3, 'grocery', 20),
+(3, 'Bread', 'Br4281', 2, 'grocery', 30),
+(4, 'Chips', 'Ch2132', 3, 'grocery', 30),
+(5, 'Juice', 'Ju8762', 2, 'grocery', 20),
+(6, 'Milk', 'Mi4562', 3, 'grocery', 20),
+(7, 'Tomato', 'To6642', 2, 'grocery', 20),
+(8, 'Camera', 'Ca7782', 200, 'technology', 20),
+(9, 'Headphones', 'He6252', 50, 'technology', 50),
+(10, 'Laptop', 'La8192', 500, 'technology', 40),
+(11, 'Phone', 'Ph0019', 300, 'technology', 50),
+(12, 'Printer', 'Pr8928', 150, 'technology', 60),
+(13, 'Speakers', 'Sp0218', 50, 'technology', 40),
+(14, 'TV', 'Tv0123', 350, 'technology', 30),
+(15, 'Hoodie', 'Ho7721', 15, 'clothes', 20),
+(16, 'Jacket', 'Ja0987', 25, 'clothes', 40),
+(17, 'Jeans', 'Je2121', 20, 'clothes', 40),
+(18, 'Shirt', 'Sh6517', 15, 'clothes', 40),
+(19, 'Shoes', 'Se0011', 30, 'clothes', 40),
+(20, 'Shorts', 'Sr0982', 25, 'clothes', 40),
+(21, 'Sunglasses', 'Su3321', 45, 'clothes', 50);
 
 INSERT INTO `productPic` (`id`, `name`, `image`) VALUES
 (1, 'Apple', 'product-images/grocery/apple.jpg'),
