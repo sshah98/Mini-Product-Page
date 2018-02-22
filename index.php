188751e935a8a1aad7f35b9b81df5ffb6ba1f811
sshah98 <shahsuraj261@gmail.com>
2018-02-21 20:30:45 -0500

    edited database and website

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ session_start();
 $connection = mysqli_connect("localhost", "suraj", "", "miniproject");
 
 //code will execute if the add to cart button is pressed
-if (isset($_POST["add_to_cart"])) {
+if (isset($_POST["addCart"])) {
+  
   //creates a new session shopping cart
   if (isset($_SESSION["shoppingCart"])) {
     
@@ -16,18 +17,17 @@ if (isset($_POST["add_to_cart"])) {
       $count = count($_SESSION["shoppingCart"]);
       $itemArray = array(
         'id'=>$_GET["id"],
-        'name'=>$_POST["hidden_name"],
-        'price'=>$_POST["hidden_price"],
-        'category'=>$_POST["hidden_category"],
+        'name'=>$_POST["name"],
+        'price'=>$_POST["price"],
+        'category'=>$_POST["category"],
         'quantity'=>$_POST["quantity"]
       );
       //keeps an indexed count in this session to keep track of how many items were added to cart
       $_SESSION["shoppingCart"][$count] = $itemArray;
-    } else {
-      // echo '<script>alert("Item already added")</script>';
-      echo 'item added';
-    }
-  } else {
+    } 
+
+  } 
+  else {
     //if there's no data, then the else block will execute
     $itemArray = array(
       'id'=>$_GET["id"],
@@ -58,9 +58,7 @@ if (isset($_GET["action"])) {
 <html>
 <head>
   <title>Suraj's Products</title>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />  
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
+  
 </head>
 <body>
   
@@ -78,7 +76,7 @@ if (isset($_GET["action"])) {
     $sql = "SELECT category FROM productInfo GROUP BY category";
     $result = mysqli_query($connection, $sql);
     
-    echo "<select name='username'>";
+    echo "<select name='category'>";
     while ($row = mysqli_fetch_array($result)) {
       echo "<option value='" . $row['category'] ."'>" . $row['category'] ."</option>";
     }
@@ -93,51 +91,47 @@ if (isset($_GET["action"])) {
     $query = "SELECT * FROM productInfo ORDER BY id ASC";
     $result = mysqli_query($connection, $query);
     
-    $query2 = "SELECT * FROM productPic ORDER BY id ASC";
+    $query2 = "SELECT * FROM productStock ORDER BY id ASC";
     $result2 = mysqli_query($connection, $query2);
-    //check to see if the database has any rows
-    if (mysqli_num_rows($result) > 0 and mysqli_num_rows($result2)) {
+    
+    //fetch all the data of the database
+    while ($row = mysqli_fetch_array($result) and $row2 = mysqli_fetch_array($result2)) {
+      ?>
       
-      //fetch all the data of the database
-      while ($row = mysqli_fetch_array($result) and $row2 = mysqli_fetch_array($result2)) {
-        ?>
-        
-        <form method="post" action="index.php?action=add&id=<?php echo $row["id"]; ?>">
-          <img src="<?php echo $row2["image"]; ?>" class="img-responsive" height="200" width="100"/>
-          <br />
-          <h4><?php echo $row["name"]; ?> </h4>
-          <h4>$<?php echo $row["price"]; ?> </h4>
-          <h4>Category: <?php echo $row["category"]; ?> </h4>
-          <h4>Stock: <?php echo $row["amount"]; ?> </h4>
-          <br />
-          <input type="number" name="quantity" class="form-control" value="1" min="1"/>  
-          <input type="hidden" name="hidden_category" value="<?php echo $row["category"]; ?>" />
-          <input type="hidden" name="hidden_name" value="<?php echo $row["name"]; ?>" />  
-          <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>" />  
-          <br />
-          <input type="submit" name="add_to_cart" style="margin-top:5px;" class="btn btn-success" value="Add to Cart" />  
-          <br />
-        </form>
-      </div>
-      
-      <?php
-    }
+      <form method="post" action="index.php?action=add&id=<?php echo $row["id"]; ?>">
+        <h4><?php echo $row["name"]; ?> </h4>
+        <h4>$<?php echo $row["price"]; ?> </h4>
+        <h4>Stock: <?php echo $row2["amount"];
+        $row2["amount"] = "UPDATE amount FROM productInfo WHERE";
+
+         ?> </h4>
+        <h4>Category: <?php echo $row["category"]; ?> </h4>
+        <input type="number" name="quantity" class="form-control" value="1" min="1"/>  
+        <input type="hidden" name="category" value="<?php echo $row["category"]; ?>" />
+        <input type="hidden" name="name" value="<?php echo $row["name"]; ?>" />  
+        <input type="hidden" name="price" value="<?php echo $row["price"]; ?>" />  
+        <br />
+        <input type="submit" name="addCart" value="Add to Cart" />  
+        <br />
+      </form>
+    </div>
+    
+    <?php
   }
   ?>     
   
   <br />
   
   <h3>Current Items</h3>
-  <table class="table table-striped table-dark">
+  <table>
     
     <thead>
       <tr>
-        <th>Item Name</th>
+        <th colspan="3">Name</th>
         <th>Quantity</th>
-        <th>Price</th>
+        <th colspan="3">Price</th>
         <th>Category</th>
-        <th>Total</th>
-        <th>Action</th>
+        <th colspan="10">Action</th>
       </tr>
     </thead>
     
@@ -148,12 +142,11 @@ if (isset($_GET["action"])) {
       foreach ($_SESSION["shoppingCart"] as $keys=>$values) {
         ?>
         <tr>
-          <td><?php echo $values["name"]; ?> </td>
-          <td><?php echo $values["quantity"]; ?> </td>
-          <td>$ <?php echo $values["price"]; ?> </td>
-          <td> <?php echo $values["category"]; ?></td>
-          <td> <?php echo $values["quantity"] * $values["price"]; ?> </td>
-          <td><a href="index.php?action=delete&id=<?php echo $values["id"]; ?>"><span class="text-danger">Remove</span></a></td>  
+          <td align="center" colspan="3"><?php echo $values["name"]; ?> </td>
+          <td align="center"><?php echo $values["quantity"]; ?> </td>
+          <td align="center" colspan="3">$ <?php echo $values["price"]; ?> </td>
+          <td align="center" colspan="3"> <?php echo $values["category"]; ?></td>
+          <td align="center" colspan="3"><a href="index.php?action=delete&id=<?php echo $values["id"]; ?>"><span class="text-danger">Remove</span></a></td>  
           
         </tr>
         <?php 
@@ -162,8 +155,8 @@ if (isset($_GET["action"])) {
       } ?>  
       
       <tr>
-        <td colspan="3" align="right">Total</td>  
-        <td align="right">$ <?php echo $total; ?></td>  
+        <td align="center"><strong>Total</strong></td>  
+        <td align="center"><strong>$ <?php echo $total; ?></strong></td>  
         <td></td>  
         
       </tr>
